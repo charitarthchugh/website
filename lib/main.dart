621d185c74aa5aa9dd1dcf79c40f672b3bf57993
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-08-21 14:34:26 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,6 @@ class MyAppState extends State<MyApp>{
     return MaterialApp(
       title: 'Charitarth Chugh',
       theme: ThemeData(
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         fontFamily: 'OpenSans',
         primaryColorDark: Color.fromRGBO(7,13, 47, 1),
         accentColor:Color.fromRGBO(247,174, 110, 1),
