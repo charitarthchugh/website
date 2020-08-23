59f8054d61cb94d0e3c6da7b30699b4a9272ef1d
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-08-23 13:35:29 -0400

    Switch to Google Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 //External Packages
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 //Internal Packages
 import 'views/profile_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({Key key}) : super(key: key);
@@ -13,21 +16,22 @@ class MyApp extends StatefulWidget {
     return new MyAppState();
   }
 }
-class MyAppState extends State<MyApp>{
+
+class MyAppState extends State<MyApp> {
   @override
   void dispose() {
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Charitarth Chugh',
-      theme: ThemeData(
-        fontFamily: 'OpenSans',
-        primaryColorDark: Color.fromRGBO(7,13, 47, 1),
-        accentColor:Color.fromRGBO(247,174, 110, 1),
-      ),
-      home: ProfilePage()
-    );
+        title: 'Charitarth Chugh',
+        theme: ThemeData(
+          textTheme: GoogleFonts.openSansTextTheme(),
+          primaryColorDark: Color.fromRGBO(7, 13, 47, 1),
+          accentColor: Color.fromRGBO(247, 174, 110, 1),
+        ),
+        home: ProfilePage());
   }
 }
