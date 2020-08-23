d8bcf8f07d197006d61746072851647ed07f6d1d
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-08-23 16:16:42 -0400

    Create darkmode.dart
    Base Widget, nothing done on it. Can be removed at any time.

---
+++ lib/components/darkmode.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Darkmode extends StatefulWidget {
+  Darkmode({Key key}) : super(key: key);
+
+  @override
+  _DarkmodeState createState() => _DarkmodeState();
+}
+
+class _DarkmodeState extends State<Darkmode> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
