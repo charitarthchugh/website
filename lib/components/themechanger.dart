5569c104a3877b662e2474226ef615d8d84befff
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-08-24 18:18:47 -0400

    Update themechanger.dart
    add getters and setterrs

--- lib/components/themechanger.dart
+++ lib/components/themechanger.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 
 class ThemeChanger extends ChangeNotifier {
   ThemeData _themeData;
-  ThemeMode _mode =ThemeMode.system;
+  ThemeMode _mode = ThemeMode.system;
 
   ThemeChanger(this._themeData);
 
   getTheme() => _themeData;
+  getMode() => _mode;
 
   setTheme(ThemeData theme) {
     _themeData = theme;
     notifyListeners();
+  }
+
+  setMode(ThemeMode mode) {
+    _mode = mode;
   }
 }
