7db7855a237973791993064bce3d163f89f16342
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-08-25 11:55:52 -0400

    Update main.dart
    Make it stateless

--- lib/main.dart
+++ lib/main.dart
@@ -25,19 +25,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key key}) : super(key: key);
-
-  MyAppState createState() {
-    return new MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  @override
-  void dispose() {
-    super.dispose();
-  }
+class MyApp extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
