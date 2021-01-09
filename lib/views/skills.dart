b017208b0ed5a804abbeda4bfb5c9bdaa55e6ca8
Charitarth Chugh <charitarth.chugh@gmail.com>
2021-01-08 22:28:22 -0500

    Create Skills.dart.

---
+++ lib/views/skills.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Skills extends StatefulWidget {  Skills({Key key}) : super(key: key);
+
+  @override
+_SkillsState createState(
+    
+  )=> _SkillsState();
+}
+
+class _SkillsState extends State<Skills> {  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
