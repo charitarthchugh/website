a29501161886124c088e9f932e2c3bce2fe2a7ef
Charitarth Chugh <37895518+charitarthchugh@users.noreply.github.com>
2022-11-15 15:19:33 -0500

    Update about.dart

--- lib/views/about.dart
+++ lib/views/about.dart
@@ -11,8 +11,8 @@ class About extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String aboutMe = "I am a sophmore at the University of Connecticut and the secretary of UConn AI Club."
-        "I am a technology enthusiast who is keen on"
+    const String aboutMe = "I am a sophomore at the University of Connecticut and the secretary of UConn AI Club."
+        "I am a technology enthusiast who is keen on "
         "sharing my knowledge to others and exploring new technologies. Programming is my passion.";
     final profileData = AutoSizeText(
       aboutMe,
